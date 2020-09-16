86128aa6f1d2128dc7ac1764605751e53f0b4b41
hcynobug <1210834761@qq.com>
2020-09-16 17:28:51 +0800

    完成了主页功能的开发

--- assets/js/baseAPI.js
+++ assets/js/baseAPI.js
@@ -5,4 +5,21 @@ $.ajaxPrefilter(function(options){
   // console.log(options.url);
   // 调用ajax的时候调用统一配置拼接路径
   options.url = 'http://ajax.frontend.itheima.net' + options.url
+
+  // 统一为有请求权限的接口设置 headers请求头
+  if (options.url.indexOf('/my/') !== -1 ){
+      options.headers ={
+        Authorization:localStorage.getItem('token') ||''
+      }
+    }
+    // 全局统一挂载 complete 回调函数
+    options.complete = function(res) {
+        // 从complete回调函数中可以利用 res.responseJSON拿到服务器响应回来的数据
+        if (res.responseJSON.status === 1 && res.responseJSON.message ==='身份认证失败！') {
+          // 强制清空 token
+          localStorage.removeItem('token')
+          // 强制跳转到首页
+          location.href = '/login.html'
+        }
+    }
 })
